1ba27058e8c95233f5f7c8372840e03d527bafad
Prashannt Bhandari <prashantbhandaricr07@gmail.com>
2026-05-03 15:47:02 +0530

    test deployment locally

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,10 +40,10 @@ app.use("/api/tasks", taskRoutes);
 
 // Production setup (Monolith)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+app.get(/.*/, (req, res) => {
+      res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
